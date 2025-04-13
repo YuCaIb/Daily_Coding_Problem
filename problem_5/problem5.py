bb71df2915d7645f4501869b51396f542beacbe8
yucaib <y.canfee@gmail.com>
2025-04-13 21:31:38 +0300

    Problem | 5 | Medium

--- problem_5/problem5.py
+++ problem_5/problem5.py
@@ -1,8 +1,27 @@
-# I still couldn't understand the question does this talks about tupels ??
+# ~ I still couldn't understand the question does this talks about tuples ?? ~
+import random
+
+
+# So the question is basically asks what it asks. It asks to write down 2 functions that will write up first element
+# of a tuple and another function that print out last element of the tuple.
+#
 def cons(a, b):
     def pair(f):
         return f(a, b)
+
     return pair
 
 
+p = cons(random.randint(a=-10, b=10), random.randint(a=-10, b=10))  # randomizing the numbers so I could tell..
 
+
+def car(pair):  # choosing the first element
+    return pair(lambda a, b: a)  # choosing a
+
+
+def cdr(pair):
+    return pair(lambda a, b: b)  # choosing b
+
+
+print(car(p))
+print(cdr(p))
